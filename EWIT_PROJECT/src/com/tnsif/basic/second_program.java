3e0c33b1b8a1a294e6fc3910cfb597722916755f
ANANYA B V PATHAR <patharananya@gmail.com>
2025-09-30 18:54:27 +0530

    second

---
+++ EWIT_PROJECT/src/com/tnsif/basic/second_program.java
@@ -0,0 +1,10 @@
+package com.tnsif.basic;
+
+public class second_program {
+	public static void main(String[] args) {
+        System.out.println("Welcome to Second program");
+
+        Sample s = new Sample(); 
+        s.display();
+	}
+}
